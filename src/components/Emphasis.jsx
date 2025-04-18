bf1dd3b437e5e4a0fccecd21c03720de38f268fa
raunak-centera <raunakt@centera.tech>
2025-04-18 16:51:21 +0530

    Updates

--- src/components/Emphasis.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Emphasis () {
-    return (
-        <div className="bg-secondary text-center text-light mb-0">
-            <p>Aqui a BLACK FRIDAY acontece o mês inteiro! Aproveite!!</p>
-        </div>
-    )
-}
